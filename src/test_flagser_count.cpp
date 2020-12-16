59a5e79d28af96c699b11d7dc71af073cc3afb21
Nicolas Ninin <nicolas.ninin@gmail.com>
2020-12-16 20:54:41 +0100

    test vanilla count

---
+++ src/test_flagser_count.cpp
@@ -0,0 +1,29 @@
+#import "flagser-count.cpp"
+
+int main(){
+
+// std::vector<vertex_index_t> row   {0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4};
+// std::vector<vertex_index_t> col{1, 3, 4, 2, 3, 4, 0, 1, 3, 4, 2, 4, 0, 2, 3};
+// int N=5;
+std::vector<vertex_index_t> row{0,0,1};
+std::vector<vertex_index_t> col{1,2,2};
+int N=3;
+auto graph = directed_graph_t(N);
+for (int i=0;i<row.size();i++)
+    graph.add_edge(row[i],col[i]);
+
+std::cout<< "Matrix"<<std::endl;
+for (vertex_index_t i=0;i<N;i++){
+    for (vertex_index_t j=0;j<N;j++){
+        if (graph.is_connected_by_an_edge(i,j))
+        {std::cout<< 1<< " ";}
+        else{std::cout<< "0 ";}
+        }
+std::cout<<std::endl;
+}
+
+grow_trees(graph);
+
+std::cout<<std::thread::hardware_concurrency()<<std::endl;
+    return 0;
+}
---
+++ src/tmp_test.py
@@ -0,0 +1,138 @@
+array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+       [1, 1, 0, 0, 0, 0, 0, 1, 0, 1]])
+
+[[0 1 0 1 1]
+ [0 0 1 1 1]
+ [1 1 0 1 1]
+ [0 0 1 0 1]
+ [1 0 1 1 0]]
+
+ [[5, 15, 24, 16, 2], [0, 0, 0, 7, 2]]
+8
+0
+0
+MAXIMAL TRUE
+[[1, 2, 3, 4],
+[1, 2, 4, 3],
+[1, 3, 2, 4],
+[1, 3, 4, 2],
+[1, 4, 2, 3],
+[1, 4, 3, 2],
+[2, 4, 0, 3],
+[4, 2, 0, 3]])
+
+
+array([[0, 1, 3, 4],
+       [0, 1, 4, 3],
+       [1, 2, 3, 4],
+       [1, 2, 4, 3],
+       [1, 3, 2, 4],
+       [1, 3, 4, 2],
+       [1, 4, 2, 3],
+       [1, 4, 3, 2],
+       [2, 0, 1, 3],
+       [2, 0, 1, 4],
+       [2, 0, 3, 4],
+       [2, 0, 4, 3],
+       [2, 1, 3, 4],
+       [2, 1, 4, 3],
+       [2, 4, 0, 3],
+       [4, 2, 0, 3]])
+array([[2, 0, 1, 3, 4],
+       [2, 0, 1, 4, 3]])
+
+
+import numpy as np
+import flagcount
+import pyflagser
+import pyflagsercount
+def unique_identifier(mat):
+    dt = np.dtype([('raw', np.void, mat.shape[1] * mat.dtype.itemsize)])
+    return mat.reshape(np.prod(mat.shape)).view(dt)
+
+
+def find_maximal_simplices_from_all(simplices, simplices_higher_dim):
+    one_index = unique_identifier(simplices)
+    simplices_higher_dim_stacked = unique_identifier(np.vstack([simplices_higher_dim[:, np.delete(np.arange(simplices_higher_dim.shape[1]), x)] for x in range(simplices_higher_dim.shape[1])]))
+    return simplices[np.logical_not(np.isin(one_index, simplices_higher_dim_stacked)), :]
+
+
+m=np.random.random((10000,10000))<0.04
+np.fill_diagonal(m,False)
+print(m.astype(int))
+print(pyflagser.flagser_count_unweighted(m))
+_,ls=flagcount.flagser(m,return_simplices=True)
+print(pyflagsercount.flagser_maximal(m))
+for i in range (len(ls)-1):
+    print(len(find_maximal_simplices_from_all(ls[-2-i],ls[-1-i])))
+
+import scipy.sparse as sparse
+sparse.coo_matrix(m).row
+sparse.coo_matrix(m).col
+
+
+[0 0 1 1 1 2 2 2 3]
+[1 2 0 2 3 0 1 3 0]
+
+[[4, 9, 10, 2], [0, 0, 6, 2]] should be 0 0 4 2
+[[0, 1],
+[0, 2],
+[1, 0],
+[1, 2],
+[1, 3],
+[2, 0],
+[2, 1],
+[2, 3],
+[3, 0]]), 
+
+[0, 1, 2],  *
+[0, 2, 1],  *
+[1, 0, 2], *
+    [1, 2, 0], *
+    [1, 2, 3],
+    [1, 3, 0],
+[2, 0, 1],
+    [2, 1, 0],
+    [2, 1, 3],
+    [2, 3, 0]
+
+[1, 2, 3, 0],
+[2, 1, 3, 0]
+
+
+
+
+array([0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 4], dtype=int32)
+array([1, 2, 3, 4, 0, 3, 4, 0, 1, 0, 1, 2], dtype=int32)
+[[5, 12, 11, 1], [0, 0, 8, 1]]
+                      0 7
+
+[[0, 1],
+[0, 2],
+[1, 0],
+[1, 2],
+[1, 3],
+[2, 0],
+[2, 1],
+[2, 3],
+[3, 0]
+[0, 1, 2],
+[0, 2, 1],
+[1, 0, 2],
+[1, 2, 0],
+[1, 2, 3],
+[1, 3, 0],
+[2, 0, 1],
+[2, 1, 0],
+[2, 1, 3],
+[2, 3, 0]]
+[1, 2, 3, 0],
+[2, 1, 3, 0]]
